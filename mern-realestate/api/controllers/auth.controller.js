409eb6cfe6fcf8c1c847c9447e238e5d32eba5f5
Siphesihle Mthethwa <51497939+Sihle-Thwa@users.noreply.github.com>
2024-03-13 00:04:30 +0200

    complete sign in page functionality

--- mern-realestate/api/controllers/auth.controller.js
+++ mern-realestate/api/controllers/auth.controller.js
@@ -1,48 +1,42 @@
-import User from '../models/user.model.js'
+import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
+import { errorHandler } from '../utils/error.js';
 
 export const signup = async (req, res, next) => {
-
     const { username, email, password } = req.body;
-    // encyrpting password using bcrypt js 
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User({ username, email, password: hashedPassword });
 
     try {
-        await newUser.save();
-        // 201 - something is bing created
-        res.status(201).json("User created successfully");
+        const hashedPassword = await bcryptjs.hash(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
 
+        await newUser.save();
+        res.status(201).json("User created successfully");
     } catch (error) {
         next(error);
     }
-}
-
-//sign in controller
+};
 
 export const signin = async (req, res, next) => {
-    //retrieve data from req.body
-
     const { email, password } = req.body;
+
     try {
-        //check if email exists
         const validUser = await User.findOne({ email: email });
 
-        //if email is not there 
-        if (!validUser) return next(errorHandler(404, "Invalid Username/Password"));
-        //check for password if user exists
-        const validPassword = bcryptjs.compareSync(password, validUser.password);
-        //if password doesn't match
-        if (!validPassword) return next(errorHandler(401, "Invalid Username/Password"));
+        if (!validUser) {
+            return next(errorHandler(404, "Invalid Username/Password"));
+        }
 
-        //authenticate the user using a cookie (save token) using jwt
+        const validPassword = await bcryptjs.compare(password, validUser.password);
+
+        if (!validPassword) {
+            return next(errorHandler(401, "Invalid Username/Password"));
+        }
+
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
         const { password: pass, ...rest } = validUser._doc;
-        res.cookie('access_token', token, { httpOnly: true })
-            .status(200)
-            .json(rest);
+
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
     } catch (error) {
         next(error);
     }
